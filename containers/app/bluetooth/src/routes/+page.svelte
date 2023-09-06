f8d51382475f32a4db4293ecb4a8405fd2f2b5e2
Willem van Heemstra <wvanheemstra@icloud.com>
2023-09-06 14:52:40 +0000

    Update

--- containers/app/bluetooth/src/routes/+page.svelte
+++ containers/app/bluetooth/src/routes/+page.svelte
@@ -1,10 +1,69 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import BluetoothAvailableIndicator from '$lib/components/BluetoothAvailableIndicator.svelte';
   import DeviceInformation from '$lib/components/DeviceInformation.svelte';
   import AccelerometerData from '$lib/components/AccelerometerData.svelte';
   import { UUIDs } from '$lib/constants/ble-uuids';
   import TemperatureSensor from '$lib/components/TemperatureSensor.svelte';
+
+  let isAvailable: boolean = false;
+  let deviceServer: BluetoothRemoteGATTServer;
+  let connectedDevice: BluetoothDevice;
+
+  onMount(() => {
+    enableBluetooth();
+  });
+
+  async function enableBluetooth(): Promise<void> {
+    try {
+      isAvailable = await navigator.bluetooth.getAvailability();
+      console.log('Bluetooth is enabled: ', isAvailable);
+      return Promise.resolve();
+    } catch (e) {
+      console.error(e);
+      isAvailable = false;
+      return Promise.reject();
+    }
+  }
+
+  async function requestBluetoothDevices(): Promise<void> {
+    try {
+      console.log('Requesting Bluetooth Devices');
+      const device = await navigator.bluetooth.requestDevice({
+        filters: [{ namePrefix: 'TI' }],
+        optionalServices: [
+          UUIDs.AccelerometerServiceUUID,
+          UUIDs.TemperatureServiceUUID,
+          UUIDs.SimpleKeyServiceUUID
+        ]
+      });
+      deviceServer = await device.gatt.connect();
+      connectedDevice = deviceServer.device;
+      return Promise.resolve();
+    } catch (e) {
+      console.error(e);
+      return Promise.reject();
+    }
+  }
 </script>
 
-<h1>Welcome to Web Bluetooth API</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container mx-auto px-4 sm:px-0">
+  <h1 class="text-3xl mt-4 mb-4">Web Bluetooth API</h1>
+
+  <button on:click={requestBluetoothDevices} disabled={!isAvailable} class="btn btn-primary">
+    Connect Bluetooth Device
+  </button>
+
+  <BluetoothAvailableIndicator {isAvailable} />
+
+  {#if isAvailable}
+    <DeviceInformation device={connectedDevice} />
+  {/if}
+
+  {#if deviceServer}
+    <div class="flex flex-row gap-4">
+      <AccelerometerData gattServer={deviceServer} />
+      <TemperatureSensor gattServer={deviceServer} />
+    </div>
+  {/if}
+</div>  
