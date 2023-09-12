873c1a10fae7fcf1e5fc006b65ac29df71452ea1
Willem van Heemstra <wvanheemstra@icloud.com>
2023-09-12 13:03:50 +0000

    Add updated store

--- containers/app/hid/svelte.config.js
+++ containers/app/hid/svelte.config.js
@@ -4,7 +4,15 @@ const config = {
 		// For the tutorial, we need to disable CSRF protection.
 		// Don't do this in your own apps unless you know what you're doing!
 		// See https://kit.svelte.dev/docs/configuration#csrf for more info.
-		csrf: false
+		csrf: false,
+		version: {
+			// ideally, this should be something deterministic
+			// like the output of `git rev-parse HEAD`
+			name: Date.now().toString(),
+
+			// if undefined, no polling will occur
+			pollInterval: 5000
+		}
 	},
 
 	vitePlugin: {
