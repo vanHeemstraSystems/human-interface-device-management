1322fd5a7627d5d7e784734d5584be42ea0df933
Willem van Heemstra <wvanheemstra@icloud.com>
2023-09-12 07:19:01 +0000

    Add database

--- containers/app/hid/src/routes/+page.svelte
+++ containers/app/hid/src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
 	export let data;
+	import { message } from '$lib/message.js';
 </script>
 
 <h1>home</h1>
 <p>this is the home page.</p>
 
 <h1>Hello {data.visited ? 'friend' : 'stranger'}!</h1>
+<p>{message}</p>
