4dbdc41f8fd05d68d8a70c219e6e401f230ca501
Willem van Heemstra <wvanheemstra@icloud.com>
2023-09-12 08:07:28 +0000

    Add to do form

--- containers/app/hid/src/routes/+page.server.js
+++ containers/app/hid/src/routes/+page.server.js
@@ -1,9 +1,30 @@
-export function load({ cookies }) {
-	const visited = cookies.get('visited');
+// export function load({ cookies }) {
+// 	const visited = cookies.get('visited');
 
-	cookies.set('visited', 'true', { path: '/' });
+// 	cookies.set('visited', 'true', { path: '/' });
+
+// 	return {
+// 		visited
+// 	};
+// }
+
+import * as db from '$lib/server/database.js';
+
+export function load({ cookies }) {
+	const id = cookies.get('userid');
+
+	if (!id) {
+		cookies.set('userid', crypto.randomUUID(), { path: '/' });
+	}
 
 	return {
-		visited
+		todos: db.getTodos(id) ?? []
 	};
 }
+
+export const actions = {
+	default: async ({ cookies, request }) => {
+		const data = await request.formData();
+		db.createTodo(cookies.get('userid'), data.get('description'));
+	}
+};
