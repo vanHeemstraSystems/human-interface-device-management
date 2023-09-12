88af992c771901d90210c2c634ed5c6531fef83b
Willem van Heemstra <wvanheemstra@icloud.com>
2023-09-12 13:08:24 +0000

    Add expected and unexpected errors

--- containers/app/hid/.svelte-kit/generated/client/app.js
+++ containers/app/hid/.svelte-kit/generated/client/app.js
@@ -8,7 +8,9 @@ export const nodes = [
 	() => import('./nodes/4'),
 	() => import('./nodes/5'),
 	() => import('./nodes/6'),
-	() => import('./nodes/7')
+	() => import('./nodes/7'),
+	() => import('./nodes/8'),
+	() => import('./nodes/9')
 ];
 
 export const server_loads = [2];
@@ -17,7 +19,9 @@ export const dictionary = {
 		"/": [~4],
 		"/about": [5],
 		"/blog": [6,[2]],
-		"/blog/[slug]": [~7,[2,3]]
+		"/blog/[slug]": [~7,[2,3]],
+		"/expected": [~8],
+		"/unexpected": [~9]
 	};
 
 export const hooks = {
